42c0bd0045896ee0606d42fab1b4301e611d6271
YamagataMiho <149132492+YamagataMiho@users.noreply.github.com>
2023-12-18 09:30:22 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,141 +1,252 @@
-
-// header領域を抜けたらnav透過
-const FixedAnimation = () => {
-	let headerHeight = $('header').height(); // セレクタ="header"の高さ取得
-	let scroll = $(window).scrollTop(); //スクロール値
-	// console.log(headerHeight)
-
-	if (scroll >= headerHeight) {
-		$('.menu ul').addClass('opacity');
-	} else {
-		$('.menu ul').removeClass('opacity');
-	};
-};
-//↑動きの詳細をまとめた関数
-//↓動かすタイミング
-
-$(window).scroll(() => {
-	FixedAnimation();
-});
-
-$(window).on('load', () => {
-	FixedAnimation();
-});
-
-// .shape全体をホバーしたときにリンク
-$('.shape,.text_01').hover(
-	function () {
-		$('.before').addClass('before_active');
-		$('.after').addClass('after_active');
-	},
-	function () {
-		$('.before').removeClass('before_active');
-		$('.after').removeClass('after_active');
-	});
-
-$('.shape,.text_01').hover(
-	function () {
-		$('.button a').css('color', '#324252');
-	},
-	function () {
-		$('.button a').css('color', '#666');
-	});
-
-
-
-// ------------------------------------
-
-$(function () {
-	$('.Toggle').click(function () {
-		$(this).toggleClass('active');
-		$('.menu').toggleClass('open');
-	});
-});
-
-$(function () {
-	var open = $('.modal-open'),
-		close = $('.modal-close'),
-		container = $('.modal-container');
-
-	open.on('click', function () {
-		container.addClass('active');
-		return false;
-	});
-
-	close.on('click', function () {
-		container.removeClass('active');
-	});
-
-	$(document).on('click', function (e) {
-		if (!$(e.target).closest('.modal-body').length) {
-			container.removeClass('active');
-		}
-	});
-});
-
-// -------------------------------------------------
-
-$(function () {
-	var open = $('.modal-open02'),
-		close = $('.modal-close'),
-		container = $('.modal-content02');
-
-	open.on('click', function () {
-		container.addClass('active');
-		return false;
-	});
-
-	close.on('click', function () {
-		container.removeClass('active');
-	});
-
-	$(document).on('click', function (e) {
-		if (!$(e.target).closest('.modal-body').length) {
-			container.removeClass('active');
-		}
-	});
-});
-
-// 申し込み手順 caption
-let duration = 500;
-$(function () {
-	$('.box').hover(slideIn, slideOut);
-});
-
-function slideIn() {
-	$('.caption1').stop().animate({ top: "50%" }, duration);
-	$('.caption2').stop().animate({ top: "60%" }, duration);
-};
-
-function slideOut() {
-	$('.caption1').stop().animate({ top: "80%" }, duration);
-	$('.caption2').stop().animate({ top: "100%" }, duration);
-};
-
-// レスポンシブ>スマホ用
-
-// 申し込み方法
-$(function () {
-	$('.captionbtn_box').on('touchstart click', function(){
-		let value = $(event.currentTarget).next().children('p');// イベント発火した要素の兄弟要素の子要素を取得
-		$(value).toggleClass('caption2_active');// 取得した要素に開閉記述
-	});
-});
-// クリックでも動作するように
-// $(function () {
-// 	$('.captionbtn_box').on('click', function(){
-// 		let value = $(event.currentTarget).next().children('p');// イベント発火した要素の兄弟要素の子要素を取得
-// 		$(value).toggleClass('caption2_active');// 取得した要素に開閉記述
-// 	});
-// });
-// 画面幅が950以下の時トップテキストを改行したい
+// 
 $(window).on('load', function () {
-	$(window).resize(function () {
-		let x = $(window).width();
-		let y = 950;
-
-		if (x <= y) {
-			$('.text_02 span').addClass('top_br')
-		};
-	});
+  const videos = $('.js_inlineVideo');
+  if (videos.length) {
+    playVideos(videos);
+    $(window).on('scroll', function () {
+      playVideos(videos);
+    });
+  }
+  function playVideos(videos) {
+    const startPosition = $(window).scrollTop() + $(window).height();
+    videos.each(function (index) {
+      if (startPosition > $(this).offset().top - 200) {
+        $(this).get(0).play();
+      }
+    });
+  }
 });
+// 
+
+
+
+
+
+
+// sectionタイトル背景バー
+function fadeAnime(){
+  
+    $('.fadeLeftTrigger').each(function(){ //fadeLeftTriggerというクラス名が
+      var elemPos = $(this).offset().top-50;//要素より、50px上の
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight){
+      $(this).addClass('fadeLeft');// 画面内に入ったらfadeLeftというクラス名を追記
+      }else{
+      $(this).removeClass('fadeLeft');// 画面外に出たらfadeLeftというクラス名を外す
+      }
+      });
+      
+    // Work boxふわっと
+    $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+      var elemPos = $(this).offset().top-50;//要素より、50px上の
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight){
+      $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+      }else{
+      $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+      }
+      });
+      // Work boxふわっと----ここまで----
+  }
+  // sectionタイトル背景バー ----ここまで----
+
+// sectionタイトル 文字パタパタ
+  var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+  
+    if (this.isDeleting) { delta /= 2; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+  
+  window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+  };
+
+  // sectionタイトル 文字パタパタ ----ここまで----
+
+// .Work modalの開閉
+  $(function(){
+    // 変数に要素を入れる
+    var open = $('.modal-open'),
+      close = $('.modal-close');
+      modalContainer = $('.modal-container');
+  
+    //開くボタンをクリックしたらモーダルを表示する
+    open.on('click',function(){	
+      var btnNo = $(this).attr("data-number");
+      var targetId = "#modal" + btnNo;
+      $(targetId).addClass("active");
+      $('body').css('overflow-y', 'hidden');
+      console.log("ccc");
+      return false;
+    });
+  
+    //閉じるボタンをクリックしたらモーダルを閉じる
+    close.on('click',function(){
+      modalContainer.removeClass("active");
+      $('body').css('overflow-y', 'auto');
+      console.log("ddd");
+      return false;
+    });
+  
+    //モーダルの外側をクリックしたらモーダルを閉じる
+    $(document).on('click',function(e) {
+      console.log("aaaa");
+      if(!$(e.target).closest('.active').length){
+        console.log("bbb");
+        modalContainer.removeClass("active");
+        $('body').css('overflow-y', 'auto');
+      }
+    });
+  });
+// .Work modalの開閉 ----ここここまで----
+
+
+
+// ----top タイトルのアニメーション----
+
+// glowAnimeにglowというクラス名を付ける定義
+function GlowAnimeControl() {
+  $('.glowAnime').each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("glow");
+
+    } else {
+      $(this).removeClass("glow");
+    }
+  });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  GlowAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  //spanタグを追加する
+    var element = $(".glowAnime");
+  element.each(function () {
+    var text = $(this).text();
+    var textbox = "";
+    text.split('').forEach(function (t, i) {
+      if (t !== " ") {
+        if (i < 10) {
+          textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
+        } else {
+        var n = i / 10;
+          textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
+        }
+
+      } else {
+        textbox += t;
+      }
+    });
+    $(this).html(textbox);
+  });
+
+  GlowAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+//
+
+// ----top タイトルのアニメーション ----ここまで----
+
+
+// -----ハンバーガーメニュー-----
+ 
+ $(function() {
+   $('.Toggle').click(function() {
+    $(this).toggleClass('active');
+   $('.menu').toggleClass('open');
+  });
+ });
+// -----ハンバーガーメニュー-----ここまで------
+
+// Top 漢字のアニメーション
+function BgFadeAnime(){
+
+$('.bgLRextendTrigger').each(function(){ //bgLRextendTriggerというクラス名が
+  var elemPos = $(this).offset().top-50;//要素より、50px上の
+  var scroll = $(window).scrollTop();
+  var windowHeight = $(window).height();
+  if (scroll >= elemPos - windowHeight){
+    $(this).addClass('bgLRextend');// 画面内に入ったらbgLRextendというクラス名を追記
+  }else{
+    $(this).removeClass('bgLRextend');// 画面外に出たらbgLRextendというクラス名を外す
+  }
+}); 
+
+ // 文字列を囲う子要素
+$('.bgappearTrigger').each(function(){ //bgappearTriggerというクラス名が
+  var elemPos = $(this).offset().top-50;//要素より、50px上の
+  var scroll = $(window).scrollTop();
+  var windowHeight = $(window).height();
+  if (scroll >= elemPos - windowHeight){
+    $(this).addClass('bgappear');// 画面内に入ったらbgappearというクラス名を追記
+  }else{
+    $(this).removeClass('bgappear');// 画面外に出たらbgappearというクラス名を外す
+  }
+});   
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function (){
+  BgFadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function(){
+  BgFadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+// Top 漢字のアニメーション ----ここまで----
